843a2725d476dbf7d2ad3aaf7b8ad6b3dd2bd547
Rohan Nicholls <rohan.nicholls@gmail.com>
2019-08-08 03:54:00 +0100

    ✨Root setup

---
+++ image-matrix/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { GridCellComponent } from './grid-cell/grid-cell.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'image-matrix';
+}
